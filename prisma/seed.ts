65bea2e419c720d7102192340df8e1224c8470a5
tkx.cool <tkx.cool@gmail.com>
2023-05-18 19:44:31 +0800

    feat: 添加prisma配置

---
+++ prisma/seed.ts
@@ -0,0 +1,13 @@
+// import { PrismaClient } from "@prisma/client";
+// const db = new PrismaClient();
+
+// async function seed() {
+//   const kody = await db.user.create({
+//     data: {
+//         email: "lizeyan1998@outlook.com",
+//       // this is a hashed version of "twixrox"
+//       password:
+//         "$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u",
+//     },
+//   });
+// }
